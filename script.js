74aff3bfd982eec1ea9220d91ef21f1c5b429a5c
cb15wss <christinebanda1@gmail.com>
2020-02-11 15:02:30 +0000

    Added event listeners

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const username = document.getElementById("username");
-console.dir(username);
-
+//console.dir(username);
+const usernameLabel = document.querySelectorAll("label")[0];
+//console.dir(usernameLabel);
 const validateUsername = event => {
   const regex = /\d\w\W+/g;
   const inputVal = event.target.value;
   if (regex.test(inputVal)) {
-    //change color of border of green
-    //display a positive message saying username accepted
+    event.target.classList.add("valid-input");
+    event.target.classList.remove("invalid-input");
+    const validMessage = document.createElement("p");
+    validMessage.classList.add("valid-p");
+    validMessage.innerText = "Great! username is available";
+    usernameLabel.appendChild(validMessage);
   } else {
-    //change color of border to red
-    //display a negative red message to say that it is invalid
+    event.target.classList.add("invalid-input");
+    event.target.classList.remove("valid-input");
+    const invalidMessage = document.createElement("p");
+    invalidMessage.classList.add("invalid-p");
+    invalidMessage.innerText = "Username is not available";
+    usernameLabel.appendChild(invalidMessage);
+  }
+};
+
+let firstClick = true;
+const removeMessages = event => {
+  if (firstClick) {
+    firstClick = false;
+  } else {
+    usernameLabel.removeChild(usernameLabel.lastChild);
   }
 };
 
 username.addEventListener("change", validateUsername);
+username.addEventListener("click", removeMessages);
